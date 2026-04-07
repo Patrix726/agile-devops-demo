ce00dbac17d80599962053a81fba8a0f5cc64ece
Nebil <a0911991679@gmail.com>
2026-04-07 11:02:13 +0300

    chore: Test workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { sum } from './utils/sum'
+import { sum } from "./utils/sum";
 
 export default function App() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>CI/CD Demo — Vite + React</h1>
-      <p>1 + 2 = {sum(1, 2)}</p>
-      <p>This demo runs tests on PRs and builds & deploys on main.</p>
-    </div>
-  )
+	return (
+		<div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+			<h1>CI/CD Demo — Vite + React</h1>
+			<p>3 + 2 = {sum(3, 2)}</p>
+			<p>This demo runs tests on PRs and builds & deploys on main.</p>
+		</div>
+	);
 }
